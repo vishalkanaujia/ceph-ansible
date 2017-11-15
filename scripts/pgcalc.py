ad3db1e77c83c41d87d13209f1cb5da0abd6f839
Vishal Kanaujia <vishal.kanaujia@flipkart.com>
2017-11-15 16:48:46 +0530

    Fixed pool creation for an empty pool string

--- scripts/pgcalc.py
+++ scripts/pgcalc.py
@@ -18,12 +18,12 @@ def populate(args):
 	calcValue=nearestPow2(math.floor((int(args[3])*int(args[1])*float(args[4]))/(100*int(args[2]))));
 	if(minValue>calcValue):
 		if args[7] == "replicated":
-			print "sudo ceph osd pool create {} {} {}_ruleset_{};sudo ceph osd pool set {} size {};sudo ceph osd pool set {} min_size {};sleep 1;".format(args[5],int(minValue),args[7],args[6],args[5],args[2],args[5],args[8])
+			print "ceph osd pool create {} {} {}_ruleset_{};ceph osd pool set {} size {};ceph osd pool set {} min_size {};sleep 1;".format(args[5],int(minValue),args[7],args[6],args[5],args[2],args[5],args[8])
 		if args[7] == "erasurecoded":
 			print "sudo ceph osd pool create {} {} erasure ecprofile {}_ruleset_{}; while [ $(sudo ceph -s | grep creating -c) -gt 0 ]; do echo -n .;sleep 1; done".format(args[5],int(minValue),args[7],args[6],args[5],args[2])
 	else:
 		if args[7] == "replicated":
-			print "sudo ceph osd pool create {} {} {}_ruleset_{};sudo ceph osd pool set {} size {};sudo ceph osd pool set {} min_size {};sleep 1;".format(args[5],int(calcValue),args[7],args[6],args[5],args[2],args[5],args[8])
+			print "ceph osd pool create {} {} {}_ruleset_{};ceph osd pool set {} size {};ceph osd pool set {} min_size {};sleep 1;".format(args[5],int(calcValue),args[7],args[6],args[5],args[2],args[5],args[8])
 		if args[7] == "erasurecoded":
 			print "sudo ceph osd pool create {} {} erasure ecprofile {}_ruleset_{}; while [ $(sudo ceph -s | grep creating -c) -gt 0 ]; do echo -n .;sleep 1; done".format(args[5],int(calcValue),args[7],args[6],args[5],args[2])
 if __name__ == "__main__":
